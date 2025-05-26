2092dd7a4f2707c51aa0eda8e0d00ffdc5990220
Jonathan Kula <jonathan@jdkula.dev>
2025-05-26 02:01:30 -0700

    slightly cleaner

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,5 @@
 mod proto;
-use std::{collections::HashMap, sync::LazyLock};
+use std::{collections::{hash_map::Entry::{Occupied, Vacant}, HashMap}, sync::LazyLock};
 
 use axum::{
     Router,
@@ -7,7 +7,7 @@ use axum::{
     routing::{get, post},
 };
 use axum_extra::protobuf::Protobuf;
-use std::sync::Mutex;
+use tokio::sync::Mutex;
 use tower_http::cors::CorsLayer;
 
 static DB: LazyLock<Mutex<HashMap<String, i64>>> = LazyLock::new(|| Mutex::new(HashMap::new()));
@@ -28,22 +28,32 @@ async fn root() -> &'static str {
     "Hello, World!"
 }
 
+fn test(v: &mut i64) {
+    *v += 1;
+}
+
 async fn accumulate(
     Protobuf(payload): Protobuf<proto::api::AccumulateRequest>,
 ) -> (StatusCode, Protobuf<proto::api::AccumulateResponse>) {
-    let mut db = DB.lock().unwrap();
-    let curr = db.get(&payload.id.clone()).copied();
-    let status = if curr.is_none() {
-        StatusCode::CREATED
-    } else {
-        StatusCode::OK
+    let mut status = StatusCode::OK;
+    let value = {
+        let mut db = DB.lock().await;
+
+        match db.entry(payload.id.clone()) {
+            Occupied(mut occupied_entry) => {
+                let val_ref = occupied_entry.get_mut();
+                *val_ref += payload.number;
+                *val_ref
+            }
+            Vacant(vacant_entry) => {
+                status = StatusCode::CREATED;
+                *vacant_entry.insert(payload.number)
+            }
+        }
     };
 
-    let value = curr.unwrap_or(0i64);
-    db.insert(payload.id.clone(), value + payload.number);
-
     let mut result = proto::api::AccumulateResponse::default();
-    result.id = payload.id.clone();
+    result.id = payload.id;
     result.so_far = value;
 
     (status, Protobuf(result))
@@ -52,20 +62,23 @@ async fn accumulate(
 async fn finish(
     Protobuf(payload): Protobuf<proto::api::FinishPipelineRequest>,
 ) -> (StatusCode, Protobuf<proto::api::FinishResponse>) {
-    let mut db = DB.lock().unwrap();
-    let curr = db.get(&payload.id.clone()).copied();
-    if curr.is_none() {
-        return (
-            StatusCode::NOT_FOUND,
-            Protobuf(proto::api::FinishResponse::default()),
-        );
+    let mut db = DB.lock().await;
+    let id = payload.id.clone();
+
+    let curr = {
+        let curr = db.remove(&id);
+        if curr.is_none() {
+            return (
+                StatusCode::NOT_FOUND,
+                Protobuf(proto::api::FinishResponse::default()),
+            );
+        };
+        curr.unwrap()
     };
 
-    db.remove(&payload.id.clone());
-
     let mut result = proto::api::FinishResponse::default();
-    result.id = payload.id.clone();
-    result.sum = curr.unwrap();
+    result.id = id;
+    result.sum = curr;
 
     (StatusCode::OK, Protobuf(result))
 }
